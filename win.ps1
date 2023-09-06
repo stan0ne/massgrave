151447b5e97d3296a79565b5bff734c5a9b3100b
stan0ne <savas_boluk86@hotmail.com>
2023-09-06 16:06:58 +0300

    Update win.ps1

--- win.ps1
+++ win.ps1
@@ -1,4 +1,4 @@
- $ErrorActionPreference = "Stop"
+$ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
