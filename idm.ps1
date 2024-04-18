77bd59747406c70877f0f674c99fd5e2f20b1685
stan0ne <savas_boluk86@hotmail.com>
2024-04-18 15:39:12 +0300

    Update idm.ps1

--- idm.ps1
+++ idm.ps1
@@ -1,15 +1,21 @@
+
 $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/stan0ne/massgrave/main/IDM.cmd'
-# $DownloadURL = 'https://raw.githubusercontent.com/WindowsAddict/IDM-Activation-Script/main/IDM.cmd'
+$DownloadURL = 'https://savasboluk.com/IDM.cmd'
+$DownloadURL2 = 'https://raw.githubusercontent.com/stan0ne/massgrave/main/IDM.cmd'
 
-$rand = Get-Random -Maximum 1000
+$rand = Get-Random -Maximum 99999999
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
-$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\IDM_$rand.cmd" } else { "$env:TEMP\IDM_$rand.cmd" }
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\IAS_$rand.cmd" } else { "$env:TEMP\IAS_$rand.cmd" }
 
-$response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
+try {
+    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
+}
+catch {
+    $response = Invoke-WebRequest -Uri $DownloadURL2 -UseBasicParsing
+}
 
 $ScriptArgs = "$args "
 $prefix = "@REM $rand `r`n"
@@ -18,5 +24,5 @@ Set-Content -Path $FilePath -Value $content
 
 Start-Process $FilePath $ScriptArgs -Wait
 
-$FilePaths = @("$env:TEMP\IDM*.cmd", "$env:SystemRoot\Temp\IDM*.cmd")
+$FilePaths = @("$env:TEMP\IAS*.cmd", "$env:SystemRoot\Temp\IAS*.cmd")
 foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
